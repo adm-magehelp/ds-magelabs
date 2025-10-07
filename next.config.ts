26b941f77fffefc6d72fc572ce26eb8e3e8e629a
Jose Neto <jmachadoaneto@gmail.com>
2025-10-07 11:01:07 -0300

    Ajustes p/ redirecionamento link storybook

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  
+  // Função de redirecionamento
+  async rewrites() {
+    return [
+      {
+        source: '/storybook', // O caminho que o usuário acessa
+        destination: '/storybook-static/index.html', // O arquivo HTML principal do Storybook
+      },
+      {
+        source: '/storybook/:path*', // Captura todos os outros arquivos e assets do Storybook
+        destination: '/storybook-static/:path*', // Mapeia-os para a pasta estática
+      },
+    ];
 };
 
 export default nextConfig;
