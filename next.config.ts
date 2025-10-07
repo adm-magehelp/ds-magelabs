c0242e730975dd8e76dfe5ba67c4af944b0d5939
Jose Neto <jmachadoaneto@gmail.com>
2025-10-07 16:06:50 -0300

    Ajustes

--- next.config.ts
+++ next.config.ts
@@ -2,26 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig = {
   // ... outras configurações
-
-  // Adicione a função rewrites
-  async rewrites() {
-    return [
-    {
-        source: '/storybook', 
-        destination: '/storybook-static/index.html', 
-      },
-      // REGRA PARA /storybook/ (barra final
-      {
-        source: '/storybook/', 
-        destination: '/storybook-static/index.html', 
-      },
-      // REGRA PARA ASSETS
-      {
-        source: '/storybook/:path*',
-        destination: '/storybook-static/:path*',
-      },
-    ];
-  },
 };
 
 export default nextConfig;
